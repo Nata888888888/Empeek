2c64b17d6985e99edb131110a0f3073167f98d6d
Nata888888888 <rozmarynovych@gmail.com>
2017-12-04 01:01:08 +0200

    Fixed autofocus

--- js/controller.js
+++ js/controller.js
@@ -8,7 +8,8 @@ function sayerController($scope, $location, $timeout, sayerModel) {
 
     $scope.showAddCommentPage = function(index) {
         $scope.selectedTopic = index;
-        $location.path('/comments')
+        $location.path('/comments');
+        $scope.new_comment_focus = true;
     }
 
     $scope.addTopic = function(title) {
@@ -40,3 +41,16 @@ function sayerController($scope, $location, $timeout, sayerModel) {
         sayerModel.deleteTopic(id);
     }
 }
+
+angular.module('sayerApp').run(function($rootScope, $timeout) {
+  // tell Angular to call this function when a route change completes
+  $rootScope.$on('$routeChangeSuccess', function() {
+    // we can't set focus at this point; the DOM isn't ready for us
+    // instead, we define a callback to be called after the $digest loop
+    $timeout(function(){
+      // once this is executed, our input should be focusable, so find (with jQuery)
+      // whatever is on the page with the autofocus attribute and focus it; fin.
+      $('[autofocus]').focus();
+    });
+  });
+}); 
